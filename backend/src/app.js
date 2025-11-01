2d6b733866a0ccb28c231c34ee9de189f26d9333
faveria <hasanfadhli699@gmail.com>
2025-11-01 20:16:25 +0700

    fixing

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,8 +27,9 @@ app.use(cors({
     'http://localhost:5173'
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Access-Control-Allow-Origin'],
+  optionsSuccessStatus: 200
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
